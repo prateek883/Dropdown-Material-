9787c65be6f9544faae4697f04ef0ebbef35ce42
prateek883 <singhprateek089@gmail.com>
2022-08-14 14:36:56 +0530

    1 file modified

--- src/dropdownFlags.js
+++ src/dropdownFlags.js
@@ -3,10 +3,6 @@ import { styled, alpha } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import EditIcon from '@mui/icons-material/Edit';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
